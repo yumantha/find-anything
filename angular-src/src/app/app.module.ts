2688f40bfa2f44fc9854a307ba1d2aba35a12e1a
Yumantha <yumanthadesilva@gmail.com>
2018-01-28 23:50:29 +0530

    basic components and routes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,23 +1,56 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "./material.module";
-import {HeaderComponent} from './components/header/header.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
 
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {HomeComponent} from './components/home/home.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {SearchResultsComponent} from './components/search-results/search-results.component';
+import {RegisterComponent} from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {AddItemComponent} from './components/add-item/add-item.component';
+import {ViewItemComponent} from './components/view-item/view-item.component';
+
+const appRoutes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'search/results', component: SearchResultsComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'items/new', component: AddItemComponent},
+  {path: 'items/:id', component: ViewItemComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    WelcomeComponent,
+    SearchResultsComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    AddItemComponent,
+    ViewItemComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
